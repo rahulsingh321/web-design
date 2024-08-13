9f611e87dd15fca4844f0a0dfe801898fd82849e
Rahul <rahul97811@gmail.com>
2024-08-14 01:05:06 +0530

    Finalize design, updated tailwind config, added images

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,7 +12,18 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        primary: '#1a9c4b',
+      }
     },
+    spacing: {
+      '1': '8px',
+      '2': '12px',
+      '3': '16px',
+      '4': '24px',
+      '5': '32px',
+      '6': '48px',
+    }
   },
   plugins: [
     require('@tailwindcss/forms'),
